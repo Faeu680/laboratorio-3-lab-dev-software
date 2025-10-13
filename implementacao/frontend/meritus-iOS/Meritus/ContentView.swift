62e4b0a01bc23cdfd2129184b660730c7d6342d8
ArthurPorto-PucMinas <ahpsilva@sga.pucminas.br>
2025-10-13 05:17:32 -0300

    feat: mobile initial setup

---
+++ implementacao/frontend/meritus-iOS/Meritus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meritus
+//
+//  Created by Arthur Porto on 13/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
